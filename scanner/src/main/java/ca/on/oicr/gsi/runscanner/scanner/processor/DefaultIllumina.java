d3b94826071df618a2ff553caa7f186a47c27cac
Heather Armstrong <heather.armstrong@oicr.on.ca>
2018-11-14 15:13:04 -0500

    update NovaSeq flowcell model

--- scanner/src/main/java/ca/on/oicr/gsi/runscanner/scanner/processor/DefaultIllumina.java
+++ scanner/src/main/java/ca/on/oicr/gsi/runscanner/scanner/processor/DefaultIllumina.java
@@ -87,7 +87,7 @@ public final class DefaultIllumina extends RunProcessor {
       FLOWCELL_PAIRED = xpath.compile("//Setup/PairEndFC/text()");
 
       XPathExpression miSeqPartNumber = xpath.compile("//FlowcellRFIDTag/PartNumber/text()");
-      XPathExpression novaSeqPartNum = xpath.compile("//FlowCellRfidTag/PartNumber/text()");
+      XPathExpression novaSeqPartNum = xpath.compile("//RfidsInfo/FlowCellMode/text()");
 
       CONTAINER_PARTNUMBER_XPATHS = Collections.unmodifiableSet(Sets.newHashSet(miSeqPartNumber, novaSeqPartNum));
     } catch (XPathExpressionException e) {
