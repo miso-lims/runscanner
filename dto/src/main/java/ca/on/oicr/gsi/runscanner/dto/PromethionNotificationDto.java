61e7e5a37876eaddf935ae576fb8f7d71249a5b9
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-02-27 15:18:03 -0500

    Revert to single Platform type

--- dto/src/main/java/ca/on/oicr/gsi/runscanner/dto/PromethionNotificationDto.java
+++ dto/src/main/java/ca/on/oicr/gsi/runscanner/dto/PromethionNotificationDto.java
@@ -5,6 +5,6 @@ import ca.on.oicr.gsi.runscanner.dto.type.Platform;
 public class PromethionNotificationDto extends NotificationDto {
   @Override
   public Platform getPlatformType() {
-    return Platform.PROMETHION;
+    return Platform.OXFORDNANOPORE;
   }
 }
