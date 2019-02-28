f9c8f5c9e118ddf628b7f5fad5a54c3bc3acba08
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-02-28 09:17:41 -0500

    Collapse 2 Nanopore DTOs into 1

--- dto/src/main/java/ca/on/oicr/gsi/runscanner/dto/NanoporeNotificationDto.java
+++ dto/src/main/java/ca/on/oicr/gsi/runscanner/dto/NanoporeNotificationDto.java
@@ -2,7 +2,7 @@ package ca.on.oicr.gsi.runscanner.dto;
 
 import ca.on.oicr.gsi.runscanner.dto.type.Platform;
 
-public class PromethionNotificationDto extends NotificationDto {
+public class NanoporeNotificationDto extends NotificationDto {
   @Override
   public Platform getPlatformType() {
     return Platform.OXFORDNANOPORE;
