efd5e9303ab8d6e16bc97470e24f8c6c7522ac46
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-04-05 14:30:41 -0400

    rename scriptPurpose -> runType

--- dto/src/main/java/ca/on/oicr/gsi/runscanner/dto/OxfordNanoporeNotificationDto.java
+++ dto/src/main/java/ca/on/oicr/gsi/runscanner/dto/OxfordNanoporeNotificationDto.java
@@ -3,18 +3,18 @@ package ca.on.oicr.gsi.runscanner.dto;
 import ca.on.oicr.gsi.runscanner.dto.type.Platform;
 
 public class OxfordNanoporeNotificationDto extends NotificationDto {
-  private String scriptPurpose;
+  private String runType;
 
   @Override
   public Platform getPlatformType() {
     return Platform.OXFORDNANOPORE;
   }
 
-  public String getScriptPurpose() {
-    return scriptPurpose;
+  public String getRunType() {
+    return runType;
   }
 
-  public void setScriptPurpose(String newPurpose) {
-    scriptPurpose = newPurpose;
+  public void setRunType(String newType) {
+    runType = newType;
   }
 }
