e25471163068acdd9e6c8d92307dccb01bd9c3b9
Alexis Varsava <17747491+avarsava@users.noreply.github.com>
2019-05-22 14:27:17 -0400

    Split MinKNOW and Protocol versions (#56)

--- dto/src/main/java/ca/on/oicr/gsi/runscanner/dto/OxfordNanoporeNotificationDto.java
+++ dto/src/main/java/ca/on/oicr/gsi/runscanner/dto/OxfordNanoporeNotificationDto.java
@@ -4,6 +4,7 @@ import ca.on.oicr.gsi.runscanner.dto.type.Platform;
 
 public class OxfordNanoporeNotificationDto extends NotificationDto {
   private String runType;
+  private String protocolVersion;
 
   @Override
   public Platform getPlatformType() {
@@ -16,5 +17,13 @@ public class OxfordNanoporeNotificationDto extends NotificationDto {
 
   public void setRunType(String newType) {
     runType = newType;
+  }
+
+  public String getProtocolVersion() {
+    return protocolVersion;
+  }
+
+  public void setProtocolVersion(String protocolVersion) {
+    this.protocolVersion = protocolVersion;
   }
 }
