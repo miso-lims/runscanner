09e6aa07e7c90749836d135da21dbdcfce738454
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-06-13 12:33:54 -0400

    Add time cutoff, refactor subdirectory check
    Don't wait until visiting files to see if directory contents are worth
    checking.
    Don't consider any files created before 2017, since they always fail
    anyway.

--- scanner/src/main/java/ca/on/oicr/gsi/runscanner/scanner/processor/BaseOxfordNanoporeProcessor.java
+++ scanner/src/main/java/ca/on/oicr/gsi/runscanner/scanner/processor/BaseOxfordNanoporeProcessor.java
@@ -12,6 +12,8 @@ import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,13 +24,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public abstract class BaseOxfordNanoporeProcessor extends RunProcessor {
+  /** Used for error and debug logging */
   private final Logger log = LoggerFactory.getLogger(BaseOxfordNanoporeProcessor.class);
+
+  /** Used for reporting non-fast5 files encountered while looking for fast5s */
   private final Logger mysteryFiles = LoggerFactory.getLogger("mysteryLogger");
 
   protected static String trackingId;
   protected static String contextTags;
   protected final String SEQUENCER_NAME;
   protected static final int LANE_COUNT = 1;
+
+  /** Skip processing anything older than 2017 since they typically fail */
+  protected static final FileTime CUTOFF_DATE =
+      FileTime.from(Instant.parse("2017-01-01T00:00:00.00Z"));
 
   protected static boolean isFileFast5(String fileName) {
     return fileName.endsWith(".fast5");
@@ -59,40 +68,65 @@ public abstract class BaseOxfordNanoporeProcessor extends RunProcessor {
       Files.walkFileTree(
           root.toPath(),
           new FileVisitor<Path>() {
+
+            /**
+             * Actions to take before visiting each individual file within the directory, mostly
+             * dealing with directory path.
+             */
             @Override
             public FileVisitResult preVisitDirectory(
                 Path path, BasicFileAttributes basicFileAttributes) throws IOException {
               log.debug("Pre-visit: " + path);
 
-              if (excludedDirectoryFormat(path)) {
+              /**
+               * If directory matches criteria we know exclude the directory from consideration,
+               * don't go into the directory (SKIP_SUBTREE)
+               */
+              if (excludedDirectoryFormat(path) || olderThanCutoff(path)) {
                 log.debug("Skipping " + path + " because we found an excluded directory in it.");
                 return FileVisitResult.SKIP_SUBTREE;
               }
+
+              /**
+               * If directory matches criteria we know include the directory, add without looking
+               * inside directory (add, SKIP_SUBTREE). Checking if result of readsDirectoryForRun
+               * 'isDirectory' is an existence check.
+               */
               if (readsDirectoryForRun(path).anyMatch(Files::isDirectory)) {
                 log.debug("Adding " + path);
                 runDirectories.add(path.toFile());
                 return FileVisitResult.SKIP_SUBTREE;
               }
-              return FileVisitResult.CONTINUE;
-            }
 
-            @Override
-            public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes)
-                throws IOException {
-              if (isFileFast5(path)) {
-                runDirectories.add(path.subpath(0, path.getNameCount() - 1).toFile());
-              } else {
-                mysteryFiles.debug(path.toString());
-                if (Arrays.asList(path.getParent().toFile().listFiles())
-                        .stream()
-                        .filter(File::isDirectory)
-                        .count()
-                    > 0) {
-                  return FileVisitResult.CONTINUE;
-                }
+              // If directory has directories or fast5s in, keep going through directory (CONTINUE)
+              if (Arrays.asList(path.toFile().listFiles())
+                      .stream()
+                      .filter(f -> f.isDirectory() || isFileFast5(f))
+                      .count()
+                  > 0) {
+                return FileVisitResult.CONTINUE;
               }
 
-              return FileVisitResult.SKIP_SIBLINGS;
+              // There's nothing good in this directory, so don't go within (SKIP_SUBTREE)
+              return FileVisitResult.SKIP_SUBTREE;
+            }
+
+            /** Actions to take on file(s) within directory. */
+            @Override
+            public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes) {
+              // If file is a fast5 file, add directory path to runDirectories
+              if (isFileFast5(path)) {
+                runDirectories.add(path.getParent().toFile());
+                return FileVisitResult.SKIP_SIBLINGS;
+              } else {
+                /**
+                 * If file isn't fast5, log because it probably shouldn't be in sequencer output,
+                 * but keep going (CONTINUE) because we know from preVisitDirectory that there's
+                 * something worth looking at in here
+                 */
+                mysteryFiles.debug(path.toString());
+                return FileVisitResult.CONTINUE;
+              }
             }
 
             @Override
@@ -118,8 +152,8 @@ public abstract class BaseOxfordNanoporeProcessor extends RunProcessor {
    * Returns true if provided Path points to a directory in a format we are skipping for
    * compatibility reasons.
    *
-   * @param path
-   * @return
+   * @param path Path to file
+   * @return true if path matches known-bad directory name format
    */
   protected abstract boolean excludedDirectoryFormat(Path path);
 
@@ -132,7 +166,7 @@ public abstract class BaseOxfordNanoporeProcessor extends RunProcessor {
    * @param runDirectory the directory to scan (which will be output from {@link
    *     #getRunsFromRoot(File)}
    * @param tz the user-specified timezone that the sequencer exists in
-   * @return
+   * @return NotificationDto containing run information
    * @throws IOException
    */
   public synchronized NotificationDto process(File runDirectory, TimeZone tz) throws IOException {
@@ -141,6 +175,8 @@ public abstract class BaseOxfordNanoporeProcessor extends RunProcessor {
             .filter(Files::isDirectory)
             .flatMap(
                 p -> {
+                  // Using walk() rather than list() prevents overlooking the case where /0 is empty
+                  // but /37 has fast5s
                   try (Stream<Path> files = Files.walk(p)) {
                     return files
                         .filter(BaseOxfordNanoporeProcessor::isFileFast5)
@@ -155,6 +191,7 @@ public abstract class BaseOxfordNanoporeProcessor extends RunProcessor {
                 })
             .findFirst()
             .orElseThrow(
+                // This can be thrown in cases of extremely large numbers of fast5s. This is OK
                 () -> new IOException("Cannot find FAST5 file in run directory: " + runDirectory));
 
     log.debug("For runDirectory = " + runDirectory + " we will be considering file: " + firstFile);
@@ -194,14 +231,27 @@ public abstract class BaseOxfordNanoporeProcessor extends RunProcessor {
       onnd.setSoftware(genericReader.string().getAttr(trackingId, "version"));
       onnd.setProtocolVersion(genericReader.string().getAttr(trackingId, "protocols_version"));
 
-      onnd.setRunType(
-          genericReader
-              .string()
-              .getAttr(
-                  trackingId, "exp_script_purpose")); // I believe this will break for minion reads
+      onnd.setRunType(genericReader.string().getAttr(trackingId, "exp_script_purpose"));
 
       additionalProcess(onnd, genericReader);
       return onnd;
+    }
+  }
+
+  /**
+   * Tests whether file at path is older than Jan 1 2017.
+   *
+   * @param path path leading to file to test
+   * @return true if file is older than Jan 1 2017 OR IOException occurs, false otherwise
+   */
+  protected boolean olderThanCutoff(Path path) {
+    try {
+      return Files.readAttributes(path, BasicFileAttributes.class)
+              .creationTime()
+              .compareTo(CUTOFF_DATE)
+          <= 0;
+    } catch (IOException e) {
+      return true;
     }
   }
 
