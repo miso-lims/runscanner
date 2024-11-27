926ad4364ebf74b9513a5d709b900bee6f3594b9
Alexis Varsava <alexis.varsava@oicr.on.ca>
2024-11-27 14:51:57 -0500

    Split samplesheet data and processing

--- dto/src/main/java/ca/on/oicr/gsi/runscanner/dto/dragen/samplesheet/Samplesheet.java
+++ dto/src/main/java/ca/on/oicr/gsi/runscanner/dto/dragen/samplesheet/Samplesheet.java
@@ -1,31 +1,15 @@
 package ca.on.oicr.gsi.runscanner.dto.dragen.samplesheet;
 
-import ca.on.oicr.gsi.runscanner.dto.type.DRAGENWorkflow;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.time.Instant;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Samplesheet {
-  private final Pattern HEADER = Pattern.compile("(?:\\[)(.*)(?:\\])");
   private List<SamplesheetSection> info;
-  private Map<DRAGENWorkflow, Boolean> expectedWorkflows = new HashMap<>();
-  static ObjectMapper mapper;
-  private static final Logger log = LoggerFactory.getLogger(Samplesheet.class);
   private Instant mtime;
 
-  public Samplesheet(ObjectMapper m, File rootDir) throws IOException {
-    mapper = m;
-    this.process(rootDir);
+  public Samplesheet() {
+    info = new LinkedList<>();
   }
 
   public SamplesheetSection getByName(String name) {
@@ -36,7 +20,7 @@ public class Samplesheet {
     return found.get(0);
   }
 
-  private void addToSamplesheet(SamplesheetSection section) {
+  public void addToSamplesheet(SamplesheetSection section) {
     SamplesheetSection potentiallyExtant = getByName(section.getName());
     if (potentiallyExtant != null) {
       info.remove(section);
@@ -44,93 +28,15 @@ public class Samplesheet {
     info.add(section);
   }
 
-  private void process(File rootDir) throws IOException {
-    // Both copies DRAGEN makes of the SampleSheet are within BCLConvert
-    // This is probably OK because we can't do any more analysis without it
-    File sampleSheet = new File(rootDir, "Data/BCLConvert/SampleSheet.csv");
-    if (sampleSheet.exists()) {
-      this.mtime = Files.getLastModifiedTime(sampleSheet.toPath()).toInstant();
-      List<String[]> lines =
-          Files.readAllLines(sampleSheet.toPath())
-              .stream()
-              .map(line -> line.split(","))
-              .filter(line -> !(line.length == 0 || line.length == 1 && line[0].isEmpty()))
-              .toList();
-
-      info = new LinkedList<>();
-      SamplesheetBCLConvertSection bclConvertSection;
-      Matcher headerMatcher;
-      String sectionName = "";
-      for (String[] line : lines) {
-        headerMatcher = HEADER.matcher(line[0]);
-        if (headerMatcher.matches()) {
-          // "Capturing groups are indexed from left to right, starting at one.
-          // Group zero denotes the entire pattern"
-          sectionName = headerMatcher.group(1);
-          continue;
-        }
-        switch (sectionName) {
-          case "BCLConvert_Data":
-            if (line[0].equals("Lane")) break; // Skip column label line
-            bclConvertSection = (SamplesheetBCLConvertSection) getByName("BCLConvert");
-            if (bclConvertSection == null) {
-              bclConvertSection = new SamplesheetBCLConvertSection();
-            }
-            bclConvertSection.addDatum(line[0], line[1], line[2], line[3]);
-            addToSamplesheet(bclConvertSection);
-            expectedWorkflows.put(DRAGENWorkflow.BCL_CONVERT, Boolean.FALSE);
-            break;
-          case "BCLConvert_Settings":
-            bclConvertSection = (SamplesheetBCLConvertSection) getByName("BCLConvert");
-            if (bclConvertSection == null) {
-              bclConvertSection = new SamplesheetBCLConvertSection();
-            }
-            bclConvertSection.addSetting(line[0], line[1]);
-            addToSamplesheet(bclConvertSection);
-            expectedWorkflows.put(DRAGENWorkflow.BCL_CONVERT, Boolean.FALSE);
-          default:
-            break;
-        }
-      }
-    } else {
-      // Samplesheet appears several hours after Analysis directory does
-      log.info("No samplesheet for {}, will look again later", rootDir);
-    }
-  }
-
   public List<SamplesheetSection> getInfo() {
     return info;
-  }
-
-  public void setWorkflowComplete(DRAGENWorkflow wf) {
-    throwForUnexpectedWorkflow(wf);
-    expectedWorkflows.put(wf, Boolean.TRUE);
-  }
-
-  public Boolean isWorkflowCompleted(DRAGENWorkflow wf) {
-    throwForUnexpectedWorkflow(wf);
-    return expectedWorkflows.get(wf);
-  }
-
-  public boolean allWorkflowsCompleted() {
-    return expectedWorkflows.values().stream().allMatch(b -> b.equals(Boolean.TRUE));
-  }
-
-  public boolean isWorkflowExpected(DRAGENWorkflow wf) {
-    return expectedWorkflows.containsKey(wf);
-  }
-
-  public boolean noWorkflowsExpected() {
-    return expectedWorkflows.isEmpty();
   }
 
   public Instant getMtime() {
     return mtime;
   }
 
-  private void throwForUnexpectedWorkflow(DRAGENWorkflow wf) {
-    if (!isWorkflowExpected(wf))
-      throw new IllegalStateException(
-          "Cannot perform operation for unexpected DRAGEN Workflow " + wf.name());
+  public void setMtime(Instant mtime) {
+    this.mtime = mtime;
   }
 }
--- scanner/src/main/java/ca/on/oicr/gsi/runscanner/scanner/processor/NovaseqXProcessor.java
+++ scanner/src/main/java/ca/on/oicr/gsi/runscanner/scanner/processor/NovaseqXProcessor.java
@@ -4,19 +4,27 @@ import ca.on.oicr.gsi.runscanner.dto.IlluminaNotificationDto;
 import ca.on.oicr.gsi.runscanner.dto.dragen.BCLConvert;
 import ca.on.oicr.gsi.runscanner.dto.dragen.DragenAnalysis;
 import ca.on.oicr.gsi.runscanner.dto.dragen.samplesheet.Samplesheet;
+import ca.on.oicr.gsi.runscanner.dto.dragen.samplesheet.SamplesheetBCLConvertSection;
 import ca.on.oicr.gsi.runscanner.dto.type.AnalysisStatus;
 import ca.on.oicr.gsi.runscanner.dto.type.DRAGENWorkflow;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NovaseqXProcessor extends DefaultIllumina {
-
+  private Map<DRAGENWorkflow, Boolean> expectedWorkflows = new HashMap<>();
+  private final Pattern HEADER = Pattern.compile("(?:\\[)(.*)(?:\\])");
   private final String NUMERAL = "\\d+";
   static ObjectMapper MAPPER = RunProcessor.createObjectMapper();
   private static final Logger log = LoggerFactory.getLogger(NovaseqXProcessor.class);
@@ -40,32 +48,32 @@ public class NovaseqXProcessor extends DefaultIllumina {
       for (File analysisAttempt : Objects.requireNonNull(analysisDir.listFiles())) {
         if (analysisAttempt.isDirectory() && analysisAttempt.getName().matches(NUMERAL)) {
           String attemptNum = analysisAttempt.getName();
-          Samplesheet samplesheet = new Samplesheet(MAPPER, analysisAttempt);
+          Samplesheet samplesheet = createSamplesheet(analysisAttempt);
           if (samplesheet.getInfo() == null) { // no info populated if samplesheet doesn't yet exist
             dto.setAnalysisStatus(AnalysisStatus.PENDING);
             return dto;
           }
 
-          if (samplesheet.noWorkflowsExpected()) {
+          if (noWorkflowsExpected()) {
             dto.setAnalysisStatus(AnalysisStatus.COMPLETED);
             return dto;
           }
 
           dragenAnalysis = new DragenAnalysis(MAPPER, samplesheet);
 
-          if (samplesheet.isWorkflowExpected(DRAGENWorkflow.BCL_CONVERT)) {
+          if (isWorkflowExpected(DRAGENWorkflow.BCL_CONVERT)) {
             BCLConvert bclConvert = new BCLConvert(samplesheet, analysisAttempt);
             dragenAnalysis.put("BCLConvert", bclConvert.getResult());
 
             // TODO: Should we not put() anything if not OK?
-            if (bclConvert.isOk()) samplesheet.setWorkflowComplete(DRAGENWorkflow.BCL_CONVERT);
+            if (bclConvert.isOk()) setWorkflowComplete(DRAGENWorkflow.BCL_CONVERT);
           }
 
           // Phase 2: more workflows go here
 
           // TODO (Phase 2): move manifest parsing here so maybe we can avoid looping over it
 
-          if (samplesheet.allWorkflowsCompleted()) {
+          if (allWorkflowsCompleted()) {
             dto.setAnalysisStatus(AnalysisStatus.COMPLETED);
           }
         }
@@ -79,5 +87,88 @@ public class NovaseqXProcessor extends DefaultIllumina {
 
   public static NovaseqXProcessor create(Builder builder, ObjectNode parameters) {
     return new NovaseqXProcessor(builder, calculateCheckOutput(parameters));
+  }
+
+  private Samplesheet createSamplesheet(File rootDir) throws IOException {
+    // Both copies DRAGEN makes of the SampleSheet are within BCLConvert
+    // This is probably OK because we can't do any more analysis without it
+    Samplesheet temp = new Samplesheet();
+    File sampleSheet = new File(rootDir, "Data/BCLConvert/SampleSheet.csv");
+    if (sampleSheet.exists()) {
+      temp.setMtime(Files.getLastModifiedTime(sampleSheet.toPath()).toInstant());
+      List<String[]> lines =
+          Files.readAllLines(sampleSheet.toPath())
+              .stream()
+              .map(line -> line.split(","))
+              .filter(line -> !(line.length == 0 || line.length == 1 && line[0].isEmpty()))
+              .toList();
+
+      SamplesheetBCLConvertSection bclConvertSection;
+      Matcher headerMatcher;
+      String sectionName = "";
+      for (String[] line : lines) {
+        headerMatcher = HEADER.matcher(line[0]);
+        if (headerMatcher.matches()) {
+          // "Capturing groups are indexed from left to right, starting at one.
+          // Group zero denotes the entire pattern"
+          sectionName = headerMatcher.group(1);
+          continue;
+        }
+        switch (sectionName) {
+          case "BCLConvert_Data":
+            if (line[0].equals("Lane")) break; // Skip column label line
+            bclConvertSection = (SamplesheetBCLConvertSection) temp.getByName("BCLConvert");
+            if (bclConvertSection == null) {
+              bclConvertSection = new SamplesheetBCLConvertSection();
+            }
+            bclConvertSection.addDatum(line[0], line[1], line[2], line[3]);
+            temp.addToSamplesheet(bclConvertSection);
+            expectedWorkflows.put(DRAGENWorkflow.BCL_CONVERT, Boolean.FALSE);
+            break;
+          case "BCLConvert_Settings":
+            bclConvertSection = (SamplesheetBCLConvertSection) temp.getByName("BCLConvert");
+            if (bclConvertSection == null) {
+              bclConvertSection = new SamplesheetBCLConvertSection();
+            }
+            bclConvertSection.addSetting(line[0], line[1]);
+            temp.addToSamplesheet(bclConvertSection);
+            expectedWorkflows.put(DRAGENWorkflow.BCL_CONVERT, Boolean.FALSE);
+          default:
+            break;
+        }
+      }
+    } else {
+      // Samplesheet appears several hours after Analysis directory does
+      log.info("No samplesheet for {}, will look again later", rootDir);
+    }
+    return temp;
+  }
+
+  public void setWorkflowComplete(DRAGENWorkflow wf) {
+    throwForUnexpectedWorkflow(wf);
+    expectedWorkflows.put(wf, Boolean.TRUE);
+  }
+
+  public Boolean isWorkflowCompleted(DRAGENWorkflow wf) {
+    throwForUnexpectedWorkflow(wf);
+    return expectedWorkflows.get(wf);
+  }
+
+  public boolean allWorkflowsCompleted() {
+    return expectedWorkflows.values().stream().allMatch(b -> b.equals(Boolean.TRUE));
+  }
+
+  public boolean isWorkflowExpected(DRAGENWorkflow wf) {
+    return expectedWorkflows.containsKey(wf);
+  }
+
+  public boolean noWorkflowsExpected() {
+    return expectedWorkflows.isEmpty();
+  }
+
+  private void throwForUnexpectedWorkflow(DRAGENWorkflow wf) {
+    if (!isWorkflowExpected(wf))
+      throw new IllegalStateException(
+          "Cannot perform operation for unexpected DRAGEN Workflow " + wf.name());
   }
 }
