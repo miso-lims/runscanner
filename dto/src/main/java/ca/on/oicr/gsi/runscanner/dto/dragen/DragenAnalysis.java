3f61c91054693e40bb60d70f19d7425ab4016624
Alexis Varsava <alexis.varsava@oicr.on.ca>
2024-11-29 12:45:01 -0500

    WorkflowAnalysis abstract class

--- dto/src/main/java/ca/on/oicr/gsi/runscanner/dto/dragen/DragenAnalysis.java
+++ dto/src/main/java/ca/on/oicr/gsi/runscanner/dto/dragen/DragenAnalysis.java
@@ -2,27 +2,12 @@ package ca.on.oicr.gsi.runscanner.dto.dragen;
 
 import ca.on.oicr.gsi.runscanner.dto.Analysis;
 import ca.on.oicr.gsi.runscanner.dto.dragen.samplesheet.Samplesheet;
-import java.util.HashMap;
-import java.util.Map;
 
 // Represents an entire attempt at a DRAGEN Pipeline
-public class DragenAnalysis implements Analysis {
+public class DragenAnalysis extends Analysis<DragenWorkflowAnalysis> {
   Samplesheet samplesheet;
-  Map<String, DragenWorkflowAnalysis> analyses = new HashMap<>();
 
   public DragenAnalysis(Samplesheet s) {
     samplesheet = s;
-  }
-
-  public DragenWorkflowAnalysis get(String workflowName) {
-    if (analyses.containsKey(workflowName)) {
-      return analyses.get(workflowName);
-    } else {
-      return new DragenWorkflowAnalysis();
-    }
-  }
-
-  public void put(String name, DragenWorkflowAnalysis dwa) {
-    analyses.put(name, dwa);
   }
 }
