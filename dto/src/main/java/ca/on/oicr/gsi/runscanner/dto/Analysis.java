48acc39aac13b0052573683a9d1e4f49974444e0
Alexis Varsava <alexis.varsava@oicr.on.ca>
2024-12-05 15:44:31 -0500

    JsonTypeInfo

--- dto/src/main/java/ca/on/oicr/gsi/runscanner/dto/Analysis.java
+++ dto/src/main/java/ca/on/oicr/gsi/runscanner/dto/Analysis.java
@@ -1,8 +1,18 @@
 package ca.on.oicr.gsi.runscanner.dto;
 
+import ca.on.oicr.gsi.runscanner.dto.dragen.DragenAnalysis;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import java.util.HashMap;
 import java.util.Map;
 
+@JsonTypeInfo(use = Id.NAME, include = As.PROPERTY, property = "suite")
+@JsonSubTypes({ //
+  @Type(value = DragenAnalysis.class, name = "DRAGEN") //
+}) //
 public abstract class Analysis<T extends WorkflowAnalysis> {
   Map<String, T> analyses = new HashMap<>();
 
