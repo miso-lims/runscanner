6e22832952fb08cf8475d0edbfd496d7f84f6b7e
Alexis Varsava <alexis.varsava@oicr.on.ca>
2025-02-04 09:05:49 -0500

    cleanup SamplesheetSection

--- scanner/src/main/java/ca/on/oicr/gsi/runscanner/scanner/processor/dragen/samplesheet/SamplesheetSection.java
+++ scanner/src/main/java/ca/on/oicr/gsi/runscanner/scanner/processor/dragen/samplesheet/SamplesheetSection.java
@@ -1,18 +1,5 @@
 package ca.on.oicr.gsi.runscanner.scanner.processor.dragen.samplesheet;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
-
-@JsonTypeInfo(use = Id.NAME, include = As.PROPERTY, property = "name")
-@JsonSubTypes({ //
-  @Type(value = SamplesheetBCLConvertSection.class, name = "BCLConvert"), //
-  @Type(value = SamplesheetReadsSection.class, name = "Reads") //
-}) //
-@JsonIgnoreProperties(ignoreUnknown = true)
 public interface SamplesheetSection {
   String getName();
 }
